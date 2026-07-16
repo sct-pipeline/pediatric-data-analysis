f06acae39ec4229b1cea46224e316e674740b6fa
Samuelle St-Onge <samuellestonge@gmail.com>
2026-07-16 15:38:23 -0400

    add script that computes the average SC tip location by age

---
+++ scripts/analysis/average_SC_tip_location.py
@@ -0,0 +1,94 @@
+import os
+import argparse
+from pathlib import Path
+import pandas as pd
+import yaml
+
+"""
+This script computes the average location of the spinal cord tip (conus medullaris), by processing the
+`interpolated_morphometrics.csv` files inside the `results/tables/morphometrics` folder.
+
+It computes both the average distance from PMJ to the conus medullaris, as well as the average position relative
+to the vertebral levels. It outputs a csv file with the average by age. 
+
+Example usage:
+    python scripts/analysis/average_SC_tip_location.py -e /path/to/exclude.yml
+""" 
+
+def main(exclude_file):
+
+    # Define the folder containing the `interpolated_morphometrics.csv` files
+    morphometrisc_folder = Path("results/tables/morphometrics/")
+
+    # Load exclude.yml file 
+    with open(exclude_file, "r") as f:
+        exclude = yaml.safe_load(f)
+
+    excluded_subjects = set(exclude.get("sc-tip", []))
+
+    # Process files ending with "interpolated_morphometrics"
+    file_suffix = "_interpolated_morphometrics.csv"     
+    age_column = "age"           
+    PMJ_distance_column = "DistancePMJ"
+    vertlevel_column = "VertLevel"
+
+    # Go through each "interpolated_morphometrics" csv file in the folder, and append to a dictionnary
+    data = []
+ 
+    for csv_file in morphometrisc_folder.glob(f"*{file_suffix}"):
+
+        # Extract subject ID (e.g. "sub-101")
+        subject = csv_file.stem.replace(file_suffix.replace(".csv", ""), "")
+
+        if subject in excluded_subjects:
+            print(f"Skipping {subject}")
+            continue
+
+        df = pd.read_csv(csv_file)
+
+        # Last row
+        last_row = df.iloc[-1]
+
+        data.append({
+            "Age": last_row[age_column],
+            "DistancePMJ": last_row[PMJ_distance_column],
+            "VertLevel": last_row[vertlevel_column]
+        })
+
+    # Compute averages per age
+
+    results = pd.DataFrame(data)
+
+    if results.empty:
+        print("No files found")
+    else:
+
+        average_by_age = (
+            results
+            .groupby("Age", as_index=False)
+            .agg(
+                mean_distance_from_PMJ=("DistancePMJ", "mean"),
+                std_distance_from_PMJ=("DistancePMJ", "std"),
+                mean_VertLevel=("VertLevel", "mean"),
+                std_VertLevel=("VertLevel", "std"),
+                N_participants=("Age", "count")
+            )
+            .sort_values("Age")
+        )
+
+        print(average_by_age)
+
+        # Save the averages to a csv 
+        output_dir = Path("results/tables/SC_tip")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_file = os.path.join(output_dir, "average_distance_PMJ_by_age.csv")
+        average_by_age.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description="Compute average spinal cord tip location by age.") 
+    parser.add_argument("-e", "--exclude-file", type=Path, required=True, help="Path to exclude.yml file") 
+    
+    args = parser.parse_args() 
+    
+    main(exclude_file=args.exclude_file)
